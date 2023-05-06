5083198941cfeb5b469e9c875d1f5a606437e874
thiago <thiagopininga@hotmail.com>
2023-05-06 01:04:18 -0300

    c123

--- conexao.php
+++ conexao.php
@@ -24,7 +24,7 @@ $DATABASE="todo";
 
   $mysqli = mysqli_init();
   $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-  $mysqli->real_connect($servername, $username, $password, 'todo');
+  $mysqli->real_connect($HOST, $USERNAME, $PASSWORD, 'todo');
   $mysqli->close();
 
 // Checando a conexão
