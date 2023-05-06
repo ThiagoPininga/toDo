631a4f16bb5040b9f91f1ccb084405e7fdb8969d
thiago <thiagopininga@hotmail.com>
2023-05-06 00:42:01 -0300

    conexao 3

--- conexao.php
+++ conexao.php
@@ -1,13 +1,32 @@
 <?php
-$hostname = 'aws.connect.psdb.cloud';
-$usuario = 'gh99poep9v5zbykumqnb';
-$senha = 'pscale_pw_WplVoDJcKskOuKMnTKzvPc2Oj6aAWDZaskdKUY9kZM2';
-$database = 'todo';
 
-$mysqli = new mysqli($hostname, $usuario, $senha, $database);
+// database: todo
+// username: iah213n2bjce2ldp12ph
+// host: aws.connect.psdb.cloud
+// password: pscale_pw_5VhgSNA6g4Uuw5kaSrBkX6oouTapsusMfbksN3AkR5T
 
-//verifica se houver erro
-if ($mysqli -> connect_errno) {
-    echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
+// $hostname = 'aws.connect.psdb.cloud';
+// $usuario = 'cohnnln8du69baxqpuq3';
+// $senha = 'pscale_pw_3RwVrjshyRFTTEuKCglbQopvNYBQIznPw1QnNJkdqjh';
+// $database = 'todo';
+
+// $mysqli = new mysqli($hostname, $usuario, $senha, $database);
+
+// //verifica se houver erro
+// if ($mysqli -> connect_errno) {
+//     echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
+// }
+
+$servername = "aws.connect.psdb.cloud";
+$username = "cohnnln8du69baxqpuq3";
+$password = "pscale_pw_3RwVrjshyRFTTEuKCglbQopvNYBQIznPw1QnNJkdqjh";
+$dbname = "todo";
+
+// Criando a conexão
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Checando a conexão
+if (!$conn) {
+    die("Falha na conexão: " . mysqli_connect_error());
 }
 ?>
