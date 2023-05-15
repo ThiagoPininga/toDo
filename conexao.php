9957fb53bce3c3c35deb489d7f94352e280bf6ad
thiago <thiagopininga@hotmail.com>
2023-05-15 09:47:35 -0300

    conexao volta

--- conexao.php
+++ conexao.php
@@ -1,14 +1,14 @@
 <?php
 
-// $hostname = 'containers-us-west-195.railway.app';
-// $usuario = 'root';
-// $senha = 'ZzivbuolTlIAoZGWkBHb';
-// $database = 'railway';
+$hostname = 'containers-us-west-195.railway.app';
+$usuario = 'root';
+$senha = 'ZzivbuolTlIAoZGWkBHb';
+$database = 'railway';
 
-// $mysqli = new mysqli($hostname, $usuario, $senha, $database);
+$mysqli = new mysqli($hostname, $usuario, $senha, $database);
 
-// //verifica se houver erro
-// if ($mysqli -> connect_errno) {
-//     echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
-// }
+//verifica se houver erro
+if ($mysqli -> connect_errno) {
+    echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
+}
 ?> 
