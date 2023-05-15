a872bde303797c197f93e47a8b312dc503c6e109
thiago <thiagopininga@hotmail.com>
2023-05-15 09:22:15 -0300

    conexao 2

--- conexao.php
+++ conexao.php
@@ -1,10 +1,5 @@
 <?php
 
-// database: todo
-// username: iah213n2bjce2ldp12ph
-// host: aws.connect.psdb.cloud
-// password: pscale_pw_5VhgSNA6g4Uuw5kaSrBkX6oouTapsusMfbksN3AkR5T
-
 $hostname = 'containers-us-west-195.railway.app';
 $usuario = 'root';
 $senha = 'ZzivbuolTlIAoZGWkBHb';
@@ -16,21 +11,4 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $database);
 if ($mysqli -> connect_errno) {
     echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
 }
-
-// $HOST="aws.connect.psdb.cloud";
-// $USERNAME="a2307nrjr2w8ir4e6av4";
-// $PASSWORD="pscale_pw_riR51gdEUBYgFNwHiaeCeOW7GsabcsehBIlatcjEDwt";
-// $DATABASE="todo";
-
-//   $mysqli = mysqli_init();
-//   $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-//   $mysqli->real_connect($HOST, $USERNAME, $PASSWORD, $DATABASE);
-//   $mysqli->close();
-
-// // Checando a conexão
-// if (!$conn) {
-//     die("Falha na conexão: " . mysqli_connect_error());
-// }
-// echo "Conexão bem-sucedida";
-
 ?>
