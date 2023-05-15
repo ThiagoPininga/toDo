2c995a5c934b4dd0f61618d5eaa7ec26d1c7d2c5
thiago <thiagopininga@hotmail.com>
2023-05-15 10:47:15 -0300

    conexao teste 2

--- conexao.php
+++ conexao.php
@@ -1,14 +1,36 @@
 <?php
 
-$hostname = 'containers-us-west-195.railway.app';
-$usuario = 'root';
-$senha = 'ZzivbuolTlIAoZGWkBHb';
-$database = 'railway';
+// $hostname = 'containers-us-west-195.railway.app';
+// $usuario = 'root';
+// $senha = 'ZzivbuolTlIAoZGWkBHb';
+// $database = 'railway';
 
-$mysqli = new mysqli($hostname, $usuario, $senha, $database);
+// $mysqli = new mysqli($hostname, $usuario, $senha, $database);
 
-//verifica se houver erro
-if ($mysqli -> connect_errno) {
-    echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
+// //verifica se houver erro
+// if ($mysqli -> connect_errno) {
+//     echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
+// }
+
+
+// Recupera a URL de conexão do banco de dados das variáveis de ambiente
+$dbUrl = getenv('DATABASE_URL');
+
+// Extrai as informações do URL
+$dbParams = parse_url($dbUrl);
+
+// Cria as variáveis com as informações de conexão
+$dbHost = $dbParams['host'];
+$dbPort = $dbParams['port'];
+$dbUser = $dbParams['user'];
+$dbPass = $dbParams['pass'];
+$dbName = ltrim($dbParams['path'], '/');
+
+// Cria uma nova conexão com o banco de dados
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
+
+// Verifica se ocorreu algum erro na conexão
+if ($mysqli->connect_error) {
+    die('Erro na conexão com o banco de dados: ' . $mysqli->connect_error);
 }
 ?> 
